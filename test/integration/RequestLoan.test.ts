c374e0c0d25a0b7af028d33fa9781f8489338769
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2024-05-12 19:50:29 -0300

    Create RequestLoan.test.ts

---
+++ test/integration/RequestLoan.test.ts
@@ -0,0 +1,36 @@
+import crypto from "crypto";
+import LogDecorator from "../../src/application/decorator/LogDecorator";
+import GetLoan from "../../src/application/usecase/GetLoan";
+import RequestLoan from "../../src/application/usecase/RequestLoan";
+import PgPromiseConnection from "../../src/infra/database/PgPromiseConnection";
+import RepositoryDatabaseFactory from "../../src/infra/factory/RepositoryDatabaseFactory";
+import RepositoryMemoryFactory from "../../src/infra/factory/RepositoryMemoryFactory";
+
+
+test("Deve aplicar para um financiamento utilizando a tabela price", async function () {
+	const code = crypto.randomUUID();
+	const connection = new PgPromiseConnection();
+	// const repositoryFactory = new RepositoryDatabaseFactory(connection);
+	const repositoryFactory = new RepositoryMemoryFactory();
+	const requestLoan = new LogDecorator(new RequestLoan(repositoryFactory));
+	const inputRequestLoan = {
+		code,
+		purchasePrice: 250000,
+		downPayment: 50000,
+		salary: 70000,
+		period: 12,
+		type: "price"
+	}
+	await requestLoan.execute(inputRequestLoan);
+	const getLoan = new LogDecorator(new GetLoan(repositoryFactory));
+	const inputGetLoan = {
+		code
+	};
+	const output = await getLoan.execute(inputGetLoan);
+	expect(output.installments).toHaveLength(12);
+	const [firstInstallment] = output.installments;
+	expect(firstInstallment.balance).toBe(184230.24);
+	const lastInstallment = output.installments[output.installments.length - 1];
+	expect(lastInstallment.balance).toBe(0);
+	connection.close();
+});
